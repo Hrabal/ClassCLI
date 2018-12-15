97b5a57666d2eb11085bf9c43eb05b37036ec49c
Hrabal <federicocerchiari@gmail.com>
2018-12-15 15:49:11 +0100

    tests refactor using only unittest

--- tests/test_first_level.py
+++ tests/test_first_level.py
@@ -5,17 +5,72 @@
 import os
 import sys
 import unittest
-from subprocess import check_output
+sys.path.append(os.getcwd())
+from classcli import CliBuilder
 
 
-class BaseUnit(unittest.TestCase):
-    def _call_cli(self, args, expected):
-        r = check_output([sys.executable, 'tests/%s' % self.TEST_CLI, ] + args)
-        self.assertEqual(r.decode('utf-8').strip(), expected)
+class TestSingleClass(unittest.TestCase):
 
+    class TestClass:
+            callable_cls = True
+            command = 'test'
 
-class TestFirstLevel(BaseUnit):
-    TEST_CLI = 't_%s' % '_'.join(__file__.split('/')[-1].split('_')[1:])
+            def _base(self):
+                return 1
 
-    def test_one_arg_base(self):
-        self._call_cli(['test', ], 'test NOK')
+            def test_method(self):
+                return 2
+
+            def test_argument(self, arg1):
+                return arg1
+
+            def test_argument_type(self, arg1: int):
+                return arg1
+
+            def test_argument_opt(self, arg1='2'):
+                return arg1
+
+            def test_argument_opt_type(self, arg1: int=2):
+                return arg1
+
+            def test_argument_opt_multiple(self, arg1='2', barg='3'):
+                return barg
+
+            def test_argument_bool(self, arg1: bool):
+                return arg1
+
+            def test_argument_bool_false(self, arg1: bool=False):
+                return arg1
+
+    def setUp(self):
+        self.parser = CliBuilder([TestSingleClass.TestClass, ])
+
+    def test_base_func(self):
+        self.assertEqual(self.parser.run_cli(['test', ]), 1)
+
+    def test_non_base_func(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_method', ]), 2)
+
+    def test_argument(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument', 3]), '3')
+
+    def test_argument_type(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_type', 3]), 3)
+
+    def test_argument_opt(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_opt', '--arg1', '1']), '1')
+
+    def test_argument_opt_type(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_opt_type', '--arg1', '1']), 1)
+
+    def test_argument_opt_alias(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_opt', '-a', '1']), '1')
+
+    def test_argument_opt_multiple(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_opt_multiple', '-b', '2']), '2')
+
+    def test_argument_bool(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_bool', '-arg1']), True)
+
+    def test_argument_bool_false(self):
+        self.assertEqual(self.parser.run_cli(['test', 'test_argument_bool_false', '-arg1']), False)
